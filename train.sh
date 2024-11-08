4521e2f4eba2b2d7224808e3d2b6dcf01fb5caf0
aoinotama <595375300@qq.com>
2024-11-08 17:50:59 +0800

    mychange

--- train.sh
+++ train.sh
@@ -16,7 +16,7 @@ network_alpha=96               # network alpha | 常用与 network_dim 相同的
 
 # Train related params | 训练相关参数
 resolution="512,768"  # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
-batch_size= 2          # batch size
+batch_size=2          # batch size
 max_train_epoches=50  # max train epoches | 最大训练 epoch
 save_every_n_epochs=1 # save every n epochs | 每 N 个 epoch 保存一次
 
